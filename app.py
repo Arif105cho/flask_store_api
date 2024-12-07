5678c3530daa6edf7cb37916f69369b223e4441e
Arif Choudhary <arif.choudhary@etelligens.in>
2024-12-07 12:02:12 +0530

    chalja bC

--- app.py
+++ app.py
@@ -475,7 +475,7 @@ api.add_resource(CreateUserVendor, '/user/create-vendor/<string:user_email>')
 def view_users():
     return render_template('users.html')
 
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True,host=0.0.0.0)
